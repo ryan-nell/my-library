da0291f531c7d60ff6e2ffa7257a1134b47d914b
Ryan Nell <ryannell@hotmail.com>
2018-11-29 21:09:58 +0000

    Add new components

---
+++ src/NewList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// This displays books to read in the future
+class NewList extends Component {
+  render() {
+    return(
+      <div>This is The new List</div>
+    )
+  }
+}
+
+export default NewList;
---
+++ src/PastList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PastList extends Component {
+  render() {
+    return(
+      <div>This is the Past List</div>
+    )
+  }
+}
+
+export default PastList;
