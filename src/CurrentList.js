a3243a5ca272ef511e5b6c3cc2736ab71b0d0f62
Ryan Nell <ryannell@hotmail.com>
2018-11-29 11:44:30 +0000

    Initial commit

---
+++ src/CurrentList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CurrentList extends Component {
+  render() {
+    return(
+      <div>This is my Current List!</div>
+    )
+  }
+}
+
+export default CurrentList;
