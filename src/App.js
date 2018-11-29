1383f24e7105be2ce80ed94debe6b9cf44000ab2
Ryan Nell <ryannell@hotmail.com>
2018-11-29 21:35:19 +0000

    Update component to display book data from the books api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import CurrentList from './CurrentList';
+import NewList from './NewList';
+import PastList from './PastList'
+import * as BooksAPI from './BooksAPI'
 
 class App extends Component {
+
+  state = {
+    books : []
+  }
+
+  // Ajax call using lifecycle hook to get the books from a local db.
+  componentDidMount() {
+    //Get all the books using a promise and set the state
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+      console.log(books[0])
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <CurrentList />
+        <CurrentList books={this.state.books}/>
+        <NewList />
+        <PastList />
       </div>
     );
   }
