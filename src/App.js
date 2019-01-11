11d6de1b365d626c26a8ab0ad61a590c024e8c68
Ryan Nell <ryannell@hotmail.com>
2019-01-11 17:53:11 +0000

    Render BookShelf component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Link, Route} from 'react-router-dom';
+
 import Bookshelf from './Bookshelf';
-import * as BooksAPI from './BooksAPI'
+import BookSearch from './BookSearch';
+import * as BooksAPI from './BooksAPI';
 
 class App extends Component {
 
+  // State object to query the books array
   state = {
-    books : []
+    books: []
   }
 
   // Ajax call using lifecycle hook to get the books from a local db.
   componentDidMount() {
     //Get all the books using a promise and set the state
     BooksAPI.getAll().then((books) => {
-      this.setState({ books })
+      this.setState({books})
     })
   }
 
   render() {
     return (
       <div className="App">
-        <div>
-          <h1 className='shelf-header'>Currently Reading</h1>
-          <Bookshelf books={this.state.books.filter( (currentBook) => (
-              currentBook.shelf === 'currentlyReading'
-            ))}
-          />
-        </div>
-        <div>
-          <h1 className='shelf-header'>Have Read</h1>
-          <Bookshelf books={this.state.books.filter( (currentBook) => (
-              currentBook.shelf === 'read'
-            ))}
-          />
-        </div>
-        <div>
-          <h1 className='shelf-header'>Want To Read</h1>
-          <Bookshelf books={this.state.books.filter( (currentBook) => (
-              currentBook.shelf === 'wantToRead'
-            ))}
-          />
-        </div>
+
+        <Route exact path='/' render={() => (
+          <div className="bookshelf-types">
+            <div>
+              <h1 className="shelf-header">Currently Reading</h1>
+              <Bookshelf books={this.state.books.filter((currentBook) => (currentBook.shelf === "currentlyReading"))}/>
+            </div>
+            <div>
+              <h1 className="shelf-header">Have Read</h1>
+              <Bookshelf books={this.state.books.filter((currentBook) => (currentBook.shelf === "read"))}/>
+            </div>
+            <div>
+              <h1 className="shelf-header">Want To Read</h1>
+              <Bookshelf books={this.state.books.filter((currentBook) => (currentBook.shelf === "wantToRead"))}/>
+            </div>
+            <Link to="/BookSearch" className="search-input">Search</Link>
+          </div>)}
+        />
+        <Route path="/booksearch" render={({history}) => (
+            <BookSearch books={this.state.books} />
+          )}
+        />
       </div>
     );
   }
