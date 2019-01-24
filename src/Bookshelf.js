1df88a38e5cd860a51c293864d24630f92ea3840
Ryan Nell <ryannell@hotmail.com>
2019-01-24 14:00:54 +0000

    Change buttons to a select dropdown menu

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -10,22 +10,8 @@ class Bookshelf extends Component {
     newShelf: PropTypes.func.isRequired
   }
 
-// Define states
-  state = {
-    showMenu: null,
-  };
-
-  // Display/Hide the menu by changing the state
-  displayMenu = id => {
-    this.setState((state) => ({
-      // Check & set the state with the id of the book or null
-      showMenu: state.showMenu === id ? null : id,
-    }));
-  };
-
   render() {
-    const { books } = this.props
-    //const author = books.authors.join(',');
+    const { books, newShelf } = this.props
 
     return(
       <div className='book-container'>
@@ -33,26 +19,17 @@ class Bookshelf extends Component {
           {books.map((book) => (
             <div className="book" key={book.id}>
               <div className="image-div">
-                <img className="book-image" src={book.imageLinks ? book.imageLinks.thumbnail: '' } alt="Book cover" />
-                <button className="shelf-button" onClick={() => this.displayMenu(book.id)} type="button">
-                  <i className="fas fa-plus-circle"></i>
-                </button>
+                <img className="book-image" src={book.imageLinks ? book.imageLinks.thumbnail : '' } alt="Book cover" />
               </div>
               <div className="shelf-action">
-                { this.state.showMenu === book.id ? (
-                    <div className='dropdown-state'>
-                      <button className="disabled-button" type="button" disabled>Move To...</button>
-                      <button onClick={(e) => {this.props.newShelf(book, e.target.value);
-                        }} className="shelf-button-option" value="none" type="button">None</button>
-                      <button onClick={(e) => {this.props.newShelf(book, e.target.value);
-                        }} className="shelf-button-option" value="currentlyReading" type="button">Currently Reading</button>
-                      <button onClick={(e) => {this.props.newShelf(book, e.target.value);
-                        }} className="shelf-button-option" value="read" type="button">Have Read</button>
-                      <button onClick={(e) => {this.props.newShelf(book, e.target.value);
-                        }} className="shelf-button-option" value="wantToRead" type="button">Want to Read</button>
-                    </div>
-                  ): null
-                }
+                <select className='dropdown-state' onChange={(e) => {newShelf(book, e.target.value);}}
+                  defaultValue={book.shelf} >
+                  <option className="shelf-select-option" value="move" disabled>Move to...</option>
+                  <option className="shelf-select-option" value="currentlyReading">Currently Reading</option>
+                  <option className="shelf-select-option" value="wantToRead">Want to Read</option>
+                  <option className="shelf-select-option" value="read">Read</option>
+                  <option className="shelf-select-option" value="none">None</option>
+                </select>
               </div>
               <div className='book-content'>
                 <p className='book-title'>{book.title}</p>
